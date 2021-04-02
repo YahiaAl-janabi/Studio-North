2a6833961eb554bc26860df6acf56ea83df5a1b8
Yahia Al-janabi <yahiaaljanabi@gmail.com>
2021-04-02 12:39:58 +0100

    up

---
+++ src/Components/App/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1>This is the heading</h1>
+      <p>This is the paragraph</p>
+    </div>
+  );
+};
+
+export default App;
