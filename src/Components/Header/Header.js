fa8dab4e982374ff4a6e00466f9943422803adb2
Yahia Al-janabi <yahiaaljanabi@gmail.com>
2021-04-05 16:50:51 +0100

    up

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,10 +1,12 @@
 import React from "react";
-import "./header.scss";
+import "./header.css";
 
 const Header = () => {
   return (
     <div>
-      <div className="header">1</div>
+      <div className="header">
+        <div className="title">Studio North</div>
+      </div>
     </div>
   );
 };
