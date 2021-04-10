5f562bf418599fdf426057a5b236c4b4a039050a
Yahia Al-janabi <yahiaaljanabi@gmail.com>
2021-04-10 22:08:24 +0100

    up

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -6,6 +6,13 @@ const Header = () => {
     <div>
       <div className="header">
         <div className="title">Studio North</div>
+        <div className="nav">
+          <ul>
+            <li>Services</li>
+            <li>Portfolio</li>
+            <li>Contact</li>
+          </ul>
+        </div>
       </div>
     </div>
   );
